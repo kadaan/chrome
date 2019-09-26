f1ce794c75fbe4e0ca3d29d8effff83e3123a57b
Joel Baranick <joel.baranick@ensighten.com>
2019-09-26 13:17:03 -0500

    Add support for incognito mode

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ const browserless = new BrowserlessServer({
   enableCors: config.ENABLE_CORS,
   enableDebugViewer: config.ENABLE_DEBUG_VIEWER,
   enableDebugger: config.ENABLE_DEBUGGER,
+  enableIncognitoMode: config.ENABLE_INCOGNITO_MODE,
   enableXvfb: config.ENABLE_XVBF,
   errorAlertURL: config.ERROR_ALERT_URL,
   exitOnHealthFailure: config.EXIT_ON_HEALTH_FAILURE,
